ae39f191a2fb124df9fb6df4c691b3215b23be8b
Nayana V <82087235+259148@users.noreply.github.com>
2021-06-14 22:05:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ st.set_option('deprecation.showfileUploaderEncoding',False) #on loading a stream
 
 @st.cache(allow_output_mutation=True) #this line prevent us from loading the model again and again and will help in storing the model in cache once it has been loaded
 
-"""def load_model(): #loading our model
-  model = tf.keras.models.load_model('BrainTumorModel .h5')
-  return model"""
+#"def load_model(): #loading our model
+ # model = tf.keras.models.load_model('BrainTumorModel .h5')
+#  return model"""
 
 model = tf.keras.models.load_model('BrainTumorModel .h5')
 #defining the header or title of the page that the user will be seeing. We also make a side bar for the web app
